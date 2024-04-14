7bc4243bc0ce650efdf301c618c81ca8f8d9ddad
nafa21 <jacknafa@gmail.com>
2024-04-15 00:59:31 +0900

    3rd commit

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,20 +3,27 @@ const conn = require('../mariadb');
 const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req, res) => {
-    let {category_id, news } = req.query;
+    let {category_id, news, limit, currentPage} = req.query;
+    // limit : 한 페이지당 보여주는 도서 수
+    // currentPage : 현재 페이지(1,2,3...)
+
+    let offset = limit * (currentPage-1);
 
     let sql = 'SELECT * FROM Bookshop.books';
     let values = [];
     if (category_id && news){
         sql += ' WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-        values = [category_id, news];
+        values = [category_id];
     } else if(category_id){
         sql += ' WHERE category_id = ?';
-        values = category_id;
+        values = [category_id];
     } else if(news) {
-        sql += ' AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-        values = news;
+        sql += ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
     }
+
+    sql += ' LIMIT ? OFFSET ?';
+    values.push(parseInt(limit), offset);
+
     conn.query(sql, values,
         (err, results) => {
             if(err) {
